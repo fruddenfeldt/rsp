7d62dcb84106738d9309d93fb1d2977478e170ad
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-05-29 12:16:03 +0200

    first iteration of JS injections

--- javascript.js
+++ javascript.js
@@ -186,8 +186,34 @@ function storePlayerChoicePaper () {
         winner();
         return;
         
-    
 }
+
+// RESULT UI:
+
+/* 
+- player should be able to play the game by clicking on buttons rather than typing their answer in a prompt.
+- For now, remove the logic that plays exactly five rounds.
+- Create three buttons, one for each selection. 
+- Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+- Add a div for displaying results and change all of your console.logs into DOM methods.
+- Display the running score, and announce a winner of the game once one player reaches 5 points.
+- You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
+
+*/ 
+
+const humanPoints = document.createElement('p')
+const computerPoints = document.createElement('p')
+const resultContainer = document.querySelector('.resultUI')
+
+humanPoints.classList.add('resultUI')
+computerPoints.classList.add('resultUI')
+
+resultContainer.appendChild(humanPoints)
+resultContainer.appendChild(computerPoints)
+
+humanPoints.textContent = 'human points!'
+computerPoints.textContent = 'computer points!'
+
 
 
 
